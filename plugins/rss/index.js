70e8c5d2835f8c65941227f7d0a404ebad74a719
Wes Todd <wes@wesleytodd.com>
2014-03-30 11:24:55 -0500

    Italia post

--- plugins/rss/index.js
+++ plugins/rss/index.js
@@ -1,8 +1,10 @@
 var rufio = require('rufio'),
 	rss = require('rss');
 
+console.log('HERE');
 rufio.onHook('afterBuild', function(compiledData, done) {
 
+	console.log('RSS Feed');
 	console.log(compiledData);
 	done();
 	
